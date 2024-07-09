1c88451e3b29b24659fe6b0f6fdaa64ea38a6160
tgodoy12 <112527761+tgodoy12@users.noreply.github.com>
2024-07-09 15:02:45 +0000

    final details and styling

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -20,27 +20,27 @@ const Home = () => {
 		}
 		
 	}
-
 	console.log(listTask);
-
-
-
-
-
 
 	return (
 		<div className="container">
+			<div className="w-50 mx-auto mt-5 text-center">
+				<p className="fs-1 m-0 text-secondary fw-bolder">TODO List</p>
+			</div>
+			
 			<div className="card w-50 mx-auto mt-5">
+				
 				<Input onChange={addTask} value={task} onKeyDown={addListTask}/>
-				
-
-				<List taskAdded={listTask} setTask={setListTask}/>
-
-				
+				<List taskAdded={listTask} setTask={setListTask}/>			
 
 				<div className="card-footer">
-    				Card footer
+					<div className="items-left">
+						<p className="text-black-50 m-0" style={{ fontSize: "smaller" }}>
+							{listTask.length} item{listTask.length !== 1 ? 's' : ''} left
+						</p> 
+					</div>
   				</div>
+
 			</div>
 		</div>
 	);
--- src/js/component/list.jsx
+++ src/js/component/list.jsx
@@ -3,28 +3,13 @@ import Delete from "./delete";
 
 
 const List = (props) => {
-
-    
-
     return (
-        
-        
         <ul className="list-group list-group-flush">
-            {props.taskAdded.map((element, index) => (
-                
-                
+            {props.taskAdded.map((element, index) => (            
                 <li key={index} className="list-group-item d-flex justify-content-between">{element}<Delete id={index} taskList={props.taskAdded} setTask={props.setTask}/></li>
-                
-                
-            ))}
-            
+            ))}         
         </ul>
-        
-       
-        
-        
     ) 
-    
 }
 
 export default List;
